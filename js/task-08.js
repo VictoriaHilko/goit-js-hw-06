b840757e7b7ff5e430cde4e18206ca635082695b
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-16 20:08:57 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+const userData = {};
+
+loginForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm (event) {
+    event.preventDefault();
+
+    const {email, password} = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert('Please, fill all the fields of form!');
+    } else {
+        userData.key = email.value;
+        userData.value = password.value;
+    }
+
+    console.log(userData);
+
+    event.currentTarget.reset();    
+}
