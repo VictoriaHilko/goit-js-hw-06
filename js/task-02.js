399efc5c8bd2862f5de0b66f122726cdec660399
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-16 16:49:17 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+for (const item of ingredients) {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.textContent = item;
+  ingredientsItem.classList.add('item');
+  ingredientsList.append(ingredientsItem);
+}
+
