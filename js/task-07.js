472bf1539d924dc1878ddae38bdc645658bb4d5e
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-16 19:57:59 +0300

    Task 7

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const sizeController = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+
+sizeController.addEventListener('input', onChange);
+
+function onChange(event){
+    console.log(event.currentTarget.value);
+    text.style.fontSize = `${event.currentTarget.value}px`;
+}
