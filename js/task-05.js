aa53dddcfb5812531ebacb366c1926b0caf8476a
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-16 18:38:01 +0300

    Task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const userName = document.querySelector('#name-input');
+const helloMessage = document.querySelector('#name-output');
+
+const emptyName = 'Anonymous';
+
+userName.addEventListener('input', onInputName);
+
+function onInputName (event) {
+    helloMessage.textContent = event.currentTarget.value || emptyName;
+}
