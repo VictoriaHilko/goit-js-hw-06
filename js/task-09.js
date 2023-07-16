df35ea2f167b4472e6b12bba7f07ed07f4b4b2f6
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-16 20:19:35 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const changeButton = document.querySelector('.change-color');
+let currentColor = document.querySelector('.color');
+const pageBody = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+changeButton.addEventListener('click', changeColor);
+
+function changeColor (event){
+  pageBody.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = pageBody.style.backgroundColor;
+}
+
